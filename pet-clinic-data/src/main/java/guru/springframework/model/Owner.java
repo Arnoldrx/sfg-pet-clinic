8efce4fbbfa9ecbbd147790e219feb02ae7c1788
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-03 13:34:15 +0100

    Initial commit

---
+++ pet-clinic-data/src/main/java/guru/springframework/model/Owner.java
@@ -0,0 +1,13 @@
+package guru.springframework.model;
+
+public class Owner extends Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
