b146f62be1c85c77e2e3a39585228ea58f8d00ab
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-10 08:44:01 +0100

    added PetTypeService. Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
