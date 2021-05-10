757a68a56e9c881f180b6e65b1ef15de0d36a522
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-10 04:05:53 +0100

    Added visits, updated owner with pet relationship. Closes #24

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Owner.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgpetclinic.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Pet> pets;
     private String name;
 
     public String getName() {
@@ -9,5 +12,13 @@ public class Owner extends Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
