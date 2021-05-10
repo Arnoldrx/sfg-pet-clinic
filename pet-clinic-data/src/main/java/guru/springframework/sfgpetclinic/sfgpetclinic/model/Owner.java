f1704c223bd66cdcb1cdc24faec15f59125fab65
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-10 04:14:57 +0100

    added properties. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Owner.java
@@ -3,15 +3,33 @@ package guru.springframework.sfgpetclinic.sfgpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
-    private String name;
 
-    public String getName() {
-        return name;
+    public String getAddress() {
+        return address;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
