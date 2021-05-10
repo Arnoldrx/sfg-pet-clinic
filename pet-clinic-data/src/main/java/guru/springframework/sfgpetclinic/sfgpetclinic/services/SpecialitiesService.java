05de98fe2153610bde9ec2f0bb9ef5a8a4e5e3e0
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-10 09:45:02 +0100

    added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
