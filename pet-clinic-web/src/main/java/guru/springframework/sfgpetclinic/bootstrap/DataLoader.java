45777457196e8533ee65fb18a92ade62df98ee7f
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-14 12:35:43 +0100

    Added spring data Jpa for visits and Loaded visit on startup bootstrap . Closes #45 Closes #46

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.sfgpetclinic.model.*;
-import guru.springframework.sfgpetclinic.sfgpetclinic.services.OwnerService;
-import guru.springframework.sfgpetclinic.sfgpetclinic.services.PetTypeService;
-import guru.springframework.sfgpetclinic.sfgpetclinic.services.SpecialityService;
-import guru.springframework.sfgpetclinic.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -88,6 +87,13 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionaCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Killy");
+
+
+         visitService.save(catVisit);
         System.out.println("Loaded Owner...");
 
         Vet vet1 =  new Vet();
