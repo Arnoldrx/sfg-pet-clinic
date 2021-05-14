a0d7c8fa1534400c438769b83ff22b29068e02d0
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-14 12:10:45 +0100

    Added map service implementation for visits. Closes #44

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
