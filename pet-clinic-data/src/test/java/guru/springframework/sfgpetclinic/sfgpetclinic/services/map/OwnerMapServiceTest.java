dff8558afc6fbf5d620558dc730a451b8d7ffaef
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-15 20:45:07 +0100

    Added test for OwnerMap Service. Closes #47

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,88 @@
+package guru.springframework.sfgpetclinic.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+
+    private Long ownerId = 1L;
+
+    private String lastName = "smith";
+
+    @BeforeEach
+    void setUp() {
+
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+
+        ownerMapService.save(Owner.builder().id(ownerId).lastName(lastName).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerMapService.findAll();
+
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(ownerId);
+
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void save() {
+
+        long owner2Id = 2L;
+        Owner owner2 = Owner.builder().id(owner2Id).build();
+
+        Owner saveOwner = ownerMapService.save(owner2);
+
+        assertEquals(owner2Id, saveOwner.getId());
+
+    }
+
+    @Test
+    void saveNoId() {
+        Owner saveOwner = ownerMapService.save(Owner.builder().build());
+        assertNotNull(saveOwner);
+        assertNotNull(saveOwner.getId());
+    }
+
+    @Test
+    void delete() {
+        ownerMapService.delete(ownerMapService.findById(ownerId));
+
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void deleteByID() {
+        ownerMapService.deleteById(ownerId);
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void findByLastName() {
+
+        Owner smith = ownerMapService.findByLastName(lastName);
+        assertNotNull(smith);
+        assertEquals(ownerId, smith.getId());
+    }
+
+    @Test
+    void findByLastNameNotFound() {
+
+        Owner smith = ownerMapService.findByLastName("foo");
+        assertNotNull(smith);
+    }
+}
