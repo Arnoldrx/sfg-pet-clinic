940261d86eaffdf431e79fc96f917dbe94344602
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-06-25 14:25:50 +0100

    added ability to add and update a visit closes #55

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Pet.java
@@ -23,7 +23,9 @@ public class Pet extends BaseEntity {
         this.petType = petType;
         this.owner = owner;
         this.birthDay = birthDay;
-        this.visits = visits;
+        if(visits == null || visits.size()>0){
+            this.visits = visits;
+        }
     }
 
     @Column(name = "name")
